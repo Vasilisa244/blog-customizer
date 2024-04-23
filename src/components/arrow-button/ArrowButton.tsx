4ac329ac5fa4d4fca2d0c2f65994e275e873b67f
Vasilisa <pro.vapro2014@yandex.ru>
2024-04-23 14:54:13 +0400

    Проект выполнен

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
 import arrow from 'src/images/arrow.svg';
-
+import clsx from 'clsx';
 import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	containerOpen: boolean;
+	onClick: () => void;
+}
+
+export const ArrowButton = ({ containerOpen, onClick }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={onClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(
+				styles.container,
+				containerOpen && styles.container_open
+			)}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, containerOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,125 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { RadioGroup } from 'components/radio-group';
+import { Select } from 'components/select';
+import { Separator } from 'components/separator';
+import { Text } from 'components/text';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+import { useState, useRef, FormEvent } from 'react';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsForm {
+	articleParams: ArticleStateType;
+	setArticleParams: (articleParams: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = ({
+	articleParams,
+	setArticleParams,
+}: ArticleParamsForm) => {
+	const [isActive, setIsActive] = useState(false);
+	const asideRef = useRef<HTMLDivElement | null>(null);
+	const [fontFamily, setFontFamily] = useState(articleParams.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleParams.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleParams.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleParams.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleParams.contentWidth);
+
+	useOutsideClickClose({
+		isOpen: isActive,
+		rootRef: asideRef,
+		onChange: setIsActive,
+	});
+
+	const handleClickOpen = () => {
+		setIsActive(!isActive);
+	};
+
+	const handleFormApply = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const newArticleParams = {
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		};
+		setArticleParams(newArticleParams);
+	};
+
+	const handleFormReset = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		setArticleParams(defaultArticleState);
+	};
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={asideRef}>
+			<ArrowButton containerOpen={isActive} onClick={handleClickOpen} />
+			<aside
+				className={clsx(styles.container, isActive && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={handleFormApply}
+					onReset={handleFormReset}>
+					<Text size={31} weight={800} uppercase={true}>
+						{'Задайте параметры'}
+					</Text>
+					<Select
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						title={'шрифт'}
+						onChange={setFontFamily}
+					/>
+					<RadioGroup
+						name={'FontSize'}
+						selected={fontSize}
+						options={fontSizeOptions}
+						title={'размер шрифта'}
+						onChange={setFontSize}
+					/>
+					<Select
+						selected={fontColor}
+						options={fontColors}
+						title={'цвет шрифта'}
+						onChange={setFontColor}
+					/>
+					<Separator></Separator>
+					<Select
+						selected={backgroundColor}
+						options={backgroundColors}
+						title={'цвет фона'}
+						onChange={setBackgroundColor}
+					/>
+					<Select
+						selected={contentWidth}
+						options={contentWidthArr}
+						title={'ширина контента'}
+						onChange={setContentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
